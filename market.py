3bcebf8f8ed90637788bee0d1cc0461792f62a33
GiorgioPisano <pisano.giorgio@yahoo.com>
2021-11-12 16:43:12 -0800

    big change

--- market.py
+++ market.py
@@ -8,7 +8,7 @@ class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
-    name = db.Column(db.String(length=12), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
 @app.route('/')
 @app.route('/home')
