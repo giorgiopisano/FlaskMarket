934407fce87c629bd1b4ddeabf131104781ac2e8
Ethan Yam <ethanjyamamoto@gmail.com>
2021-11-28 21:28:56 -0800

    a

--- market/routes.py
+++ market/routes.py
@@ -6,6 +6,11 @@ from market import db
 from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
+
+@app.route('/home#')
+def description_page():
+    return render_template('description.html')
+    
 @app.route('/home')
 def home_page():
     return render_template('home.html')
